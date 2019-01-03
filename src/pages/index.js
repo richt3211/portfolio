5b12cd2fb754f45ab3e42b4fa30b0c8c0409aaef
Richard Timpson <richardtimpson80@gmail.com>
2019-01-02 21:01:39 -0700

    Making placeholder for live site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import Header from '../components/header';
+import styles from './index.module.css';
 
 export default () => (
-    <Header>
-        <h1>This is the header</h1>
-        <h2>This is the smaller header</h2>
-    </Header>
+    <div className= {styles.site}>
+        <Header>
+            <h1>This is the header</h1>
+            <h2>This is the smaller header</h2>
+        </Header>
+        <h1 className = {styles.header} >Portfolio site coming soon!</h1>
+    </div>
 )
+
